fc9a34f3630df90587987557a90b170f93a7aaa4
xou1i <https://github.com/xou1i>
2025-12-20 17:53:13 +0300

    add API

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,55 @@
 <script>
-  my.getAuthCode({
-    scopes: ["auth_base", "USER_ID"],
-    success: (res) => {
-      my.alert({
-        content: res.authCode,
-      });
-    },
-    fail: (res) => {
-      console.log(res.authErrorScopes);
-    },
-  });
+  // @ts-nocheck
+
+  let isLoading = false;
+
+  function loginWithSuperQi() {
+    isLoading = true;
+
+    // Call SuperQi/Hylid Bridge to get Auth Code
+    // @ts-ignore
+    my.getAuthCode({
+      scopes: ["auth_base", "USER_ID"],
+      success: (res) => {
+        // Exchange Auth Code for Token via API
+        fetch("https://its.mouamle.space/api/auth-with-superQi", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            code: res.authCode,
+          }),
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            console.log("Login success:", data);
+            // @ts-ignore
+            my.alert({
+              content: "Login Successful: " + JSON.stringify(data),
+            });
+          })
+          .catch((error) => {
+            console.error("Login API Error:", error);
+            // @ts-ignore
+            my.alert({
+              content: "Login Failed: " + error.message,
+            });
+          })
+          .finally(() => {
+            isLoading = false;
+          });
+      },
+      fail: (res) => {
+        console.error("Auth Code Error:", res);
+        isLoading = false;
+        // @ts-ignore
+        my.alert({
+          content: "Auth Failed: " + JSON.stringify(res),
+        });
+      },
+    });
+  }
 </script>
 
 <main>
@@ -42,20 +82,14 @@
       <button
         class="btn btn-primary"
         type="button"
-        onClick={() =>
-          my.getAuthCode({
-            scopes: ["auth_base", "USER_ID"],
-            success: (res) => {
-              my.alert({
-                content: res.authCode,
-              });
-            },
-            fail: (res) => {
-              console.log(res.authErrorScopes);
-            },
-          })}
+        on:click={loginWithSuperQi}
+        disabled={isLoading}
       >
-        Continue
+        {#if isLoading}
+          Signing In...
+        {:else}
+          Continue with SuperQi
+        {/if}
       </button>
     </div>
   </div>
